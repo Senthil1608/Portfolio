426551b914c10b827940bb74d53fc9ebffb4757e
Senthil1608 <108118222+Senthil1608@users.noreply.github.com>
2024-08-04 14:43:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      oswald: ["Oswald", 'sans-serif'],
+      pompiere: ["Pompiere", 'sans-serif'],
+      junge: ["Junge", 'cursive'],
+      jura:["Jura", 'sans-serif']
+    },
+    
+  },
+  plugins: [],
+}
